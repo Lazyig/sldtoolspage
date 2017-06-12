17af79dcb44cdff07866aa28bd92a33b47bb4c24
kim19 <kimig009@gmail.com>
2017-06-12 15:15:13 +0900

    fda

--- recipe/msg.js
+++ recipe/msg.js
@@ -1,3 +1,3 @@
-window.addEventListener('DOMContentLoaded', function() {
-    kigui.layout.header.msg.setEvent('1.13.9 버전이 업데이트 되었습니다.', 'afsdsaf', true);
+﻿window.addEventListener('DOMContentLoaded', function() {
+    kigui.layout.header.msg.setEvent('변경된 페이지를 즐겨찾기 해주세요.', 'adfsdsaf', true);
 })
