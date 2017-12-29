7c27f7c712f24c1c7f301717e9be35152e4ba447
ㅁㄴㅇㄹ <kimig009@gmail.com>
2017-12-29 23:16:14 +0900

    Update msg.js
    공지 사항 변경

--- recipe/msg.js
+++ recipe/msg.js
@@ -1,3 +1,3 @@
 ﻿window.addEventListener('DOMContentLoaded', function() {
-    kigui.layout.header.msg.setEvent('변경된 페이지를 즐겨찾기 해주세요.', 'adfsdsaf', true);
+    kigui.layout.header.msg.setEvent('안드로이드 어플도 있습니다. 플레이스토어에 검색!.', 'adfsdsaf', true);
 })
