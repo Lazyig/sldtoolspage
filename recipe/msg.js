0a9c51295317ca2ab210209e7eb3df0da71d0859
19 <kimig009@gmail.com>
2019-04-02 00:17:47 +0900

    ㅇㅇ?

--- recipe/msg.js
+++ recipe/msg.js
@@ -1,3 +1,1 @@
-﻿window.addEventListener('DOMContentLoaded', function() {
-    kigui.layout.header.msg.setEvent('2019/04/01 살모사 조합법 누락사항 수정', 'new2312a2', true);
-});
+window.addEventListener("DOMContentLoaded",function(){kigui.layout.header.msg.setEvent("2019/04/01 살모사 조합법 누락사항 수정","new2312a2",!0)});
